37191b65e4bf495ecf9e1fdd00d42f47bc3fa840
cristopherolivares <cristopherolivares90@outlook.com>
2021-03-19 17:27:50 -0600

    trabajo día 19/03/21

---
+++ src/conceptosbasicos/ConceptosBasicos.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conceptosbasicos;
+
+import ico.fes.Automovil;
+import java.awt.Color;
+
+/**
+ *
+ * @author cristopher
+ */
+public class ConceptosBasicos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Automovil miBocho= new Automovil();
+        miBocho.setMarca("VW");
+        miBocho.setSubMarca("Sedan");
+        miBocho.setModelo(1970);
+        miBocho.setColor(Color.blue);
+        System.out.println(miBocho);
+        //TAREA programar los objetos miAcura y miMustang 
+    }
+    
+}
